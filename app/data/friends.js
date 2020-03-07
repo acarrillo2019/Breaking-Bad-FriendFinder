3281b15673eee45ffb9cbf5d3ba3798559ba2d7f
acarrillo2019 <alric21@aol.com>
2020-03-07 10:27:16 -0700

    added characters

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,28 +5,59 @@ var friendArray = [
     {
       friendName: "Hank",
       photo: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_hank-schrader-lg.jpg",
-      scores: [4,5,5,5,5,1,2,3,4,5]
+      scores: [1,5,5,5,1,1,1,1,2,5]
     },
     {
         friendName: "Walter",
         photo: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
-        scores: [1,2,3,4,5,4,4,4,4,4]
+        scores: [1,5,1,5,1,5,1,5,4,5]
     },
     {
         friendName: "Jesse",
         photo: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_jesse-pinkman-lg.jpg",
-        scores: [2,3,4,5,5,1,2,3,4,5]
+        scores: [5,4,4,1,1,4,7,3,5,5]
     },
     {
         friendName: "Gus",
         photo: "https://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2012/10/bb-s4-giancarlo-esposito-profile-560.jpg",
-        scores: [1,2,3,4,5,5,5,5,5,5]
+        scores: [1,5,3,3,3,3,1,3,3,1]
     },
     {
         friendName: "Mike",
         photo: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_mike-ehrmantraut-lg.jpg",
         scores: [2,2,2,2,2,2,2,2,2,2]
-    }
+    },
+    {
+        friendName: "Tuco Salamanca",
+        photo: "https://vignette.wikia.nocookie.net/breakingbad/images/a/a7/Tuco_BCS.jpg/revision/latest?cb=20170810082445",
+        scores: [3,5,3,1,5,5,4,3,1,5]
+    },
+    {
+        friendName: "Hector Salamanca",
+        photo: "https://pbs.twimg.com/profile_images/378800000297781379/6395cb24dc09c37c938ffc541d0290d4_400x400.jpeg",
+        scores: [1,3,1,1,5,2,5,1,1,5]
+    },
+    {
+        friendName: "Gail Boettinger",
+        photo: "https://vignette.wikia.nocookie.net/breakingbad/images/a/ab/Boetticher.jpg/revision/latest?cb=20130724202014",
+        scores: [3,5,1,5,1,3,1,5,4,4]
+    },
+    {
+        friendName: "Saul Goodman",
+        photo: "https://vignette.wikia.nocookie.net/breakingbad/images/1/16/Saul_Goodman.jpg/revision/latest?cb=20120704065846",
+        scores: [1,5,1,5,1,2,1,5,5,5]
+    },
+    {
+        friendName: "Jane Margolis",
+        photo: "https://vignette.wikia.nocookie.net/to-hollywood-and-beyond/images/6/60/Jane_Margolis.jpg/revision/latest/scale-to-width-down/340?cb=20150510215215",
+        scores: [5,2,5,1,1,4,3,1,3,3]
+    },
+    {
+        friendName: "Skyler White",
+        photo: "https://vignette.wikia.nocookie.net/breakingbad/images/b/be/Skyler_2009.jpg/revision/latest?cb=20191126212830",
+        scores: [1,5,1,5,1,5,1,4,4,3]
+    },
+
   ];
   
   // Export the array.
