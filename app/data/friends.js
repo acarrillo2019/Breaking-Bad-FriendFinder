b8a7b9e0a1d153587e1e238bd8b2f2b624648c90
acarrillo2019 <alric21@aol.com>
2020-03-07 08:27:46 -0700

    Minor changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,27 +5,27 @@ var friendArray = [
     {
       friendName: "Hank",
       photo: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_hank-schrader-lg.jpg",
-      scores: [4,5,6,7,8]
+      scores: [4,5,5,5,5,1,2,3,4,5]
     },
     {
         friendName: "Walter",
         photo: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
-        scores: [1,2,3,4,5]
+        scores: [1,2,3,4,5,4,4,4,4,4]
     },
     {
         friendName: "Jesse",
         photo: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_jesse-pinkman-lg.jpg",
-        scores: [2,3,4,5,6]
+        scores: [2,3,4,5,5,1,2,3,4,5]
     },
     {
         friendName: "Gus",
         photo: "https://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2012/10/bb-s4-giancarlo-esposito-profile-560.jpg",
-        scores: [1,2,3,4,5]
+        scores: [1,2,3,4,5,5,5,5,5,5]
     },
     {
         friendName: "Mike",
         photo: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_mike-ehrmantraut-lg.jpg",
-        scores: [7,8,9,10,11]
+        scores: [2,2,2,2,2,2,2,2,2,2]
     }
   ];
   
